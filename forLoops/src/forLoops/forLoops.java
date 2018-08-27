d381a6f870173b1f15cadfeeda50bf068fef3ff4
eric.rapp <eric.rapp@MU-C1-01.mullen.pvt.k12.co.us>
2018-08-27 08:56:30 -0600

    Finsihed All For Loops Challenges

--- forLoops/src/forLoops/forLoops.java
+++ forLoops/src/forLoops/forLoops.java
@@ -3,27 +3,51 @@ package forLoops;
 public class forLoops {
 
 	public static void main(String[] args) {
-		challengeOne();
-		challengeTwo();
+		//challengeOne();
+		//challengeTwo();
+		//challengeThree();
+		//challengeFour();
 	}
 
-	
+
+
+
+
+
+
+
+
 	private static void challengeOne() {
 		for(int i = 1; i <= 5; i++)
-			System.out.println(i);
-	}
-	private static void challengeTwo() {
-		for(int i = 0; i <= 10; i = i + 2)
 		{
-			for(int t = 2; t <= 10; t = t + 2)
+			System.out.println(i);
+		}
+	}
+	
+	private static void challengeTwo() {
+		for(int i = 2; i <= 10; i = i + 2)
+		{
+			
+			System.out.println(i + " I love Mullen!");
+		}
+	}
+	private static void challengeThree() {
+			for(int i =  1; i <=5; i++)
 			{
-			System.out.println(t + " I love Mullen!");
-		}
-		}
+				System.out.print(i + " ");
+			}
+	}	
+	private static void challengeFour() {
+			for(int i = 10; i >= 0; i--)
+			{
+				System.out.println(i);
+				
+			}
+				System.out.println("Lift Off!");
+			}
+	
 	}
-
-
-	}
+	
 
 
 
