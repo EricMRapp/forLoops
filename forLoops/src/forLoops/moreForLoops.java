926289ec0ba261a2c9690d17b8eb8fe657705e9e
eric.rapp <eric.rapp@MU-C1-01.mullen.pvt.k12.co.us>
2018-08-27 09:06:59 -0600

    Attempting CChallenge One of more for loops

---
+++ forLoops/src/forLoops/moreForLoops.java
@@ -0,0 +1,20 @@
+package forLoops;
+
+public class moreForLoops {
+
+	public static void main(String[] args) {
+		challengeOne();
+		
+
+	}
+
+	private static void challengeOne() {
+		for(int i = 5; i <= 100; i = i + 5)
+		{
+			int sum = i + i;
+			System.out.println();
+		}
+	
+	
+	}
+}
