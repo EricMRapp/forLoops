391036f81ed6c1e6c40b10f66a0aa5afa4100a90
eric.rapp <eric.rapp@MU-C1-01.mullen.pvt.k12.co.us>
2018-08-30 08:50:45 -0600

    TwTwo done

--- forLoops/src/forLoops/moreForLoops.java
+++ forLoops/src/forLoops/moreForLoops.java
@@ -1,20 +1,50 @@
 package forLoops;
 
+import java.util.Scanner;
+
 public class moreForLoops {
 
 	public static void main(String[] args) {
-		challengeOne();
-		
-
+		//challengeOne();
+		//challengeTwo();
+		challengeThree();
 	}
 
+	
 	private static void challengeOne() {
+		int sum = 0;
 		for(int i = 5; i <= 100; i = i + 5)
 		{
-			int sum = i + i;
-			System.out.println();
+			sum = sum + i;
+			
+		
 		}
-	
+		System.out.println("The sum of all the numbers between 5-100 by multiples of 5 is " +sum );
 	
 	}
+	private static void challengeTwo()
+	{
+	System.out.println("Please give a lower bound number");
+	Scanner s1 = new Scanner(System.in);
+	int lower = s1.nextInt();
+	System.out.println("Please give a higher bound nubmer");
+	Scanner s2 = new Scanner(System.in);
+	int higher = s2.nextInt();
+	int sum = 0;
+	for(int i = lower; i <= higher; i++)
+	{
+		sum = sum + i;
+		
+	}
+	System.out.println(sum);
+	}
+	private static void challengeThree() {
+		int sum = 0;
+		for(int i = 0; i <= 100; i++)
+		{
+			//nice
+		}
+		
+	}
+
 }
