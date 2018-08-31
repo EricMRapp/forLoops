3c05d2186cd0cade5d69e38c22d409ce13c37bb7
eric.rapp <eric.rapp@MU-C1-01.mullen.pvt.k12.co.us>
2018-08-31 09:03:44 -0600

    Challenge Three is finished

--- forLoops/src/forLoops/moreForLoops.java
+++ forLoops/src/forLoops/moreForLoops.java
@@ -2,6 +2,8 @@ package forLoops;
 
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class moreForLoops {
 
 	public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class moreForLoops {
 	}
 
 	
+	
+
+
 	private static void challengeOne() {
 		int sum = 0;
 		for(int i = 5; i <= 100; i = i + 5)
@@ -38,13 +43,27 @@ public class moreForLoops {
 	}
 	System.out.println(sum);
 	}
+	
 	private static void challengeThree() {
+		System.out.println("Please give a lower bound number");
+		Scanner s1 = new Scanner(System.in);
+		int lower = s1.nextInt();
+		System.out.println("Please give a higher bound nubmer");
+		Scanner s2 = new Scanner(System.in);
+		int higher = s2.nextInt();
 		int sum = 0;
-		for(int i = 0; i <= 100; i++)
+		for(; lower <= higher; lower++)
 		{
-			//nice
+			if(lower % 3 == 0)
+				{
+				sum = sum + lower;
+				
+				}
+			
 		}
-		
-	}
-
+		System.out.println(sum);
+		}
 }
+	
+	
+
